fc41dda8adff5669fcddfb163cce938ef294bd48
xjpzt666-lang <xjpzt666@gmail.com>
2026-07-29 07:46:53 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // 告诉打包机去下面这个旧仓库找 Xposed 的核心库！
+        maven { url = uri("https://jcenter.bintray.com") } 
     }
 }
 
