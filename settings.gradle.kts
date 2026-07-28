b55a23960d83e96c673c400ede746d4d8c7607de
xjpzt666-lang <xjpzt666@gmail.com>
2026-07-29 07:37:54 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
-    @Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
