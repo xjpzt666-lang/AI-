e2fe94f68bca716e61c5f07f756c9dbb0f595dd8
xjpzt666-lang <xjpzt666@gmail.com>
2026-07-29 06:01:29 +0900

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.aihellotalk"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.aihellotalk"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 3
+        versionName = "3.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    // Xposed API (compileOnly，运行时不打包)
+    compileOnly("de.robv.android.xposed:api:82")
+    
+    // OkHttp 4.x（打包进APK）
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
